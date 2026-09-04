0617f4f52b34af9bc806e46eae5f5a991ba2e178
Priya Ravichandran <priyaravichandran283@gmail.com>
2026-09-04 22:52:30 +0530

    binary search

---
+++ Linear-Searching/BinarySearch.java
@@ -0,0 +1,46 @@
+package Searching;
+
+public class BinarySearch {
+
+	public static void main(String[] args) {
+
+		int[] arr= {-18,-12,-4,0,2,3,4,15,18,22,45,89};
+		int target=-18;
+		int ans=binarySearch(arr,target);
+		System.out.println(ans);
+		
+	}
+	//return the index
+	//return -1 if it does not exist
+	
+	static int binarySearch(int[] arr, int target)
+	{
+		int start=0;
+		int end=arr.length-1;
+		
+		while(start <= end)
+		{
+			//find the middle elemeny
+			// int mid=(stat+end) / 2; // might be possible that (start+ end)exceed the range of integer in java 
+			int mid=start+(end-start)/2;
+			
+			if(target < arr[mid])
+			{
+				end=mid-1;
+			}
+			else if(target > arr[mid])
+			{
+				start=mid+1;
+			}
+			else
+			{
+				//ans found
+				return mid;
+			}
+		}
+		
+		return -1;
+		
+	}
+
+}
